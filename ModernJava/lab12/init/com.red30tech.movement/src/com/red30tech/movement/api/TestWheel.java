60f34360b898afa50b899ccb53f4f53794be66ec
Nick <info@thoughtflow.ca>
2019-01-02 22:19:06 -0500

    Initial seeding

---
+++ ModernJava/lab12/init/com.red30tech.movement/src/com/red30tech/movement/api/TestWheel.java
@@ -0,0 +1,21 @@
+package com.red30tech.movement.api;
+
+/**
+ * This is the Test class for Wheel.
+ */
+public class TestWheel {
+
+  public static void main(String... args) {
+    Wheel wheel = Wheel.buildStandardWheel();
+
+    while (wheel.getVelocityInKph() < 100) {
+       System.out.println("increasing speed..." + (int) wheel.getVelocityInKph() + " " + (int) wheel.getRpm());
+       wheel.increaseRpm();
+    }
+
+    while (wheel.getVelocityInKph() > 0) {
+       System.out.println("decreasing speed..." + (int) wheel.getVelocityInKph() + " " + (int) wheel.getRpm());
+       wheel.decreaseRpm();
+    }
+  }
+}
