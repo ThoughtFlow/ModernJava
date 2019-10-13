b0a360bfc191db53db5fcdc4d91d4c92ad398758
Nick Maiorano <maioranonick@gmail.com>
2019-10-13 12:45:22 -0400

    Renamed class

--- ModernJava/src/chap12/StackInstrumentationDemo.java
+++ ModernJava/src/chap12/StackInstrumentationDemo.java
@@ -5,7 +5,7 @@ import java.util.List;
 import java.util.function.Consumer;
 import java.util.stream.Collectors;
 
-public class StackWalkingTest {
+public class StackInstrumentationDemo {
 
     private static void walkingTest() {
         System.out.println("Walking test");
