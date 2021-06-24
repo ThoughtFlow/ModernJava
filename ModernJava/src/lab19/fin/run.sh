8cbe4fe258f49853f92f48fb1e6aff0b37f0fa9d
Nick <nick@doclinc.me>
2021-06-24 16:15:50 +0200

    Corrected path

--- ModernJava/src/lab19/fin/run.sh
+++ ModernJava/src/lab19/fin/run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-javac -d /tmp/lab05 ../../lab05/fin/Infinite.java
+javac -d /tmp/lab19 ../../lab19/fin/Infinite.java
 
-java -Xlog:gc+heap=debug:file=/tmp/GC.log -Xms100M -Xmx200m -XX:+UseG1GC  -cp /tmp/lab05 lab05.fin.Infinite
-java -Xlog:gc+heap=debug:file=/tmp/GC.log -Xms100M -Xmx200m -XX:+UnlockExperimentalVMOptions -XX:+UseEpsilonGC  -cp /tmp/lab05 lab05.fin.Infinite
+java -Xlog:gc+heap=debug:file=/tmp/GC.log -Xms100M -Xmx200m -XX:+UseG1GC  -cp /tmp/lab19 lab19.fin.Infinite
+java -Xlog:gc+heap=debug:file=/tmp/GC.log -Xms100M -Xmx200m -XX:+UnlockExperimentalVMOptions -XX:+UseEpsilonGC  -cp /tmp/lab19 lab19.fin.Infinite
