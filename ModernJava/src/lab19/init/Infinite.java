a52f2529c6f217729e3a5c1d730e4ae9a8093ea1
Nick <nick@doclinc.me>
2021-07-25 12:00:31 -0400

    Added init package to GC lab

---
+++ ModernJava/src/lab19/init/Infinite.java
@@ -0,0 +1,16 @@
+package lab19.init;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Infinite {
+    public static void main(String[] args) {
+        List<String> list =
+                IntStream.iterate(1, i -> ++i).
+                        mapToObj(Integer::toString).
+                        map(s -> s + s).
+                        collect(Collectors.toList());
+        System.out.println(list.size());
+    }
+}
--- ModernJava/src/lab19/init/run.sh
+++ ModernJava/src/lab19/init/run.sh
@@ -2,14 +2,14 @@
 
 javac -d /tmp/lab19 ../../lab19/fin/Infinite.java
 
-# Configured to:
+# Currently set to default GC. Change the setting accordingly:
 #   a) Use the G1 garbage collector
 #   b) Turn on debug heap profiling and output to file /tmp/GC.log
 #   c) Set the minimum heap to 100Mb and the max heap to 200Mb
-java -Xlog:gc+heap=debug:file=/tmp/GC.log -Xms100M -Xmx200m -XX:+UseG1GC  -cp /tmp/lab19 lab19.fin.Infinite
+java -cp /tmp/lab19 lab19.fin.Infinite
 
-# Configured to:
+# Currently set to default GC. Change the setting accordingly:
 #   a) Use the epsilon garbage collector
 #   b) Turn on debug heap profiling and output to file /tmp/GC.log
 #   c) Set the minimum heap to 100Mb and the max heap to 200Mb
-java -Xlog:gc+heap=debug:file=/tmp/GC.log -Xms100M -Xmx200m -XX:+UnlockExperimentalVMOptions -XX:+UseEpsilonGC  -cp /tmp/lab19 lab19.fin.Infinite
+java -cp /tmp/lab19 lab19.fin.Infinite
